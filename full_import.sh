9d6a308cea9e210afa8c639bf661660d72e74fc0
Noel Chiasson <noel@discoverygarden.ca>
2022-05-19 10:16:26 -0300

    Updating tail script to follow puppet process

--- full_import.sh
+++ full_import.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo "## Running .bash_alias and .vimrc import"
 sh /Users/noelchiasson/bin/transfer_bash_vim.sh ${1}
+
 echo "## Running import of CSV Test Files"
 sh /Users/noelchiasson/bin/import_data_aws.sh ${1}
+
 echo "## ssh-ing into environment"
-#ssh ${1}
 if [[ ${2} == "tail" ]]
   then
-    ssh -t ${1} 'tail -f /var/log/cloud-init-output.log; bash -l'
+    # Old way - ssh -t ${1} 'tail -f /var/log/cloud-init-output.log; bash -l'
+    ssh -t ${1} 'tail --pid=$(pgrep -x puppet) -f /var/log/cloud-init-output.log; bash -l'
   else
     ssh ${1}
 fi
