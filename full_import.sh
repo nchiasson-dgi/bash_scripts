fe754ee8011eeda29416cea2e63d524534bc9e8b
Noel Chiasson <noel@discoverygarden.ca>
2022-05-19 13:37:01 -0300

    Adding waiting functions

--- full_import.sh
+++ full_import.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+until host ${1} &> /dev/null
+do
+  echo "Waiting for environment..."
+  sleep 20
+done
+
 echo "## Running .bash_alias and .vimrc import"
 sh /Users/noelchiasson/bin/transfer_bash_vim.sh ${1}
 
@@ -9,7 +16,7 @@ echo "## ssh-ing into environment"
 if [[ ${2} == "tail" ]]
   then
     # Old way - ssh -t ${1} 'tail -f /var/log/cloud-init-output.log; bash -l'
-    ssh -t ${1} 'tail --pid=$(pgrep -x puppet) -f /var/log/cloud-init-output.log; bash -l'
+    ssh -t ${1} 'until preg -x puppet ; do echo "Waiting for Puppet process to exist" ; sleep 10 ; done ; tail --pid=$(pgrep -x puppet) -f /var/log/cloud-init-output.log; bash -l'
   else
     ssh ${1}
 fi
